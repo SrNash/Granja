7055205b51c88c333ed74b219777200019ee5c00
Full Name <victor_rivero_90@hotmail.com>
2022-11-25 13:51:28 +0000

    Update AnimationsUI.cs

--- FarmProject/Assets/AnimationsUI.cs
+++ FarmProject/Assets/AnimationsUI.cs
@@ -4,5 +4,61 @@ using UnityEngine;
 
 public class AnimationsUI : MonoBehaviour
 {
-    
+    [Header("GameObjects")]
+    public GameObject go;
+
+    [Header("Tiempos")]
+    [SerializeField]
+    float timeScaleUp;
+    [SerializeField]
+    float timeScaleDown;
+    [SerializeField]
+    float timeColorChange;
+
+    [Header("Escalados")]
+    [SerializeField]
+    Vector3 scaleUp;
+    [SerializeField]
+    Vector3 scaleDown;
+
+    [Header("Materiales")]
+    [SerializeField]
+    Color defColor;
+    [SerializeField]
+    Color hitColor;
+
+    [Header("Comprobador")]
+    [SerializeField]
+    bool clicked = false;
+
+    private void Start()
+    {
+        go = this.gameObject;
+    }
+
+    public void Hittedgo()
+    {
+        if (!clicked)
+        {
+            clicked = true;
+            ScaledUp();
+        }
+        else if (clicked)
+        {
+            clicked = false;
+            ScaleDown();
+        }
+    }
+    public void ScaledUp()
+    {
+        LeanTween.scale(go, scaleUp, timeScaleUp).setEaseOutBounce();
+        go.LeanColor(hitColor, timeColorChange).setEaseInOutQuart();
+    }
+    public void ScaleDown()
+    { 
+        LeanTween.scale(go, scaleDown, timeScaleDown).setEaseInBounce();
+        go.LeanColor(defColor, timeColorChange).setEaseInOutQuart();
+    }
+}
+
 }
